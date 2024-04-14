47ce0c565df2100d4cb0be96ffc8792d8e1d8667
Florian <florian.atteneder@gmail.com>
2024-04-14 14:47:36 +0200

    add outputdir argument to script.jl

--- script.jl
+++ script.jl
@@ -2,9 +2,21 @@ time_mpi = @elapsed @eval using MPI
 MPI.Init()
 comm = MPI.COMM_WORLD
 rank = MPI.Comm_rank(comm)
+
+if length(ARGS) < 1
+    if rank == 0
+        println("""usage: script.jl "outputdir" """)
+    end
+    MPI.Barrier(comm)
+    exit(-1)
+end
+
+MPI.Barrier(comm)
 time_ode = @elapsed @eval using OrdinaryDiffEq
 MPI.Barrier(comm)
-open(joinpath(@__DIR__, "time_$rank"), "w") do io
+
+outputdir = ARGS[1]
+open(joinpath(outputdir, "time_$rank"), "w") do io
     println(io, "MPI,", time_mpi)
     println(io, "OrdinaryDiffEq,", time_ode)
 end
